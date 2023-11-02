8be8d59104e4d1d78ed9dd851120046ad843b799
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2023-11-02 09:40:20 +0100

    commit messgae

---
+++ index.js
@@ -0,0 +1,9 @@
+const startEL = document.getElementById("start");
+const stopEL = document.getElementById("stop");
+const resetEL = document.getElementById("reset");
+
+
+
+startEL.addEventListener("click", )
+stopEL.addEventListener("click", )
+resetEL.addEventListener("click", )
